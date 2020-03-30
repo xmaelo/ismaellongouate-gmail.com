3ba0058282065ec13f29f75b52b2c63fae0ed358
I-foletia <ismaellongouate@gmail.com>
2020-03-30 18:55:24 +0100

    irst commitd

--- index.php
+++ index.php
@@ -2,43 +2,7 @@
 
 
 
-function lister($chemin)
-{
-   echo '<u>Contenu du dossier '.$chemin.'</u><br><blockquote>';
-   
-   //nom du r?pertoire ? lister
-   $nom_repertoire = $chemin;
-   
-   //on ouvre un pointeur sur le repertoire
-   $pointeur = opendir($nom_repertoire);
-   
-   //pour chaque fichier et dossier
-   while ($fichier = readdir($pointeur))
-   {
-      //on ne traite pas les . et ..
-      if(($fichier != '.') && ($fichier != '..'))
-      {
-         //si c'est un dossier, on le lit
-         // if (is_dir($nom_repertoire.'/'.$fichier))
-         // {
-         //    echo '<blockquote>'; 
-         //    lister($nom_repertoire.'/'.$fichier);
-         //    echo '</blockquote>';
-         // }
-         // else
-         // {
-            //c'est un fichier, on l'affiche
-         // }
-      }
-            echo $fichier.'<br>';
-   }
-   echo '</blockquote><u>Fin du dossier '.$nom_repertoire.'</u><br>';
-   
-   //fermeture du pointeur
-   closedir($pointeur);
-}
-lister(__DIR__);
-lister(__DIR__.'/application');
+
 //die();
 
 
